e1ea8caab6ba906ac6a704f19a24d4e98a2901ff
rroyson <frontiersons@gmail.com>
2017-09-07 15:05:50 -0400

    added paragraph styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,12 +13,36 @@ const Home = function() {
             className="vh-100 w-100 bg-dark-gray white cover flex flex-column"
             style={{
               backgroundImage:
-                'url("https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/10644981_1529062614008312_4558642661077359055_n.jpg?oh=8ff6acd9e247ae2bc7dfcf6e0fcf4e8f&oe=5A574D53")'
+                'url("https://static.wixstatic.com/media/f2a084_0bef4e0fafa8409b8dc7528fef0ff5ac.jpg/v1/fill/w_385,h_480,al_c,q_80,usm_0.66_1.00_0.01/f2a084_0bef4e0fafa8409b8dc7528fef0ff5ac.webp")'
             }}
           >
-            <div className="o-60">
+            <div className="o-70">
               <Header />
 
+            </div>
+            <div className="center flex-column mt6 w-75 tc bg-white-80 black avenir">
+              <article>
+                <h1 className="pa2">
+                  {' '}hello paragraph{' '}
+                </h1>
+                <p className="ml3 tl">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus
+                  qui blanditiis praesentium voluptatum deleniti atque corrupti
+                  quos dolores et quas molestias excepturi sint occaecati
+                  cupiditate non provident, similique sunt in culpa qui officia
+                  deserunt mollitia animi, id est laborum et dolorum fuga. Et
+                  harum quidem rerum facilis est et expedita distinctio. Nam
+                  libero tempore, cum soluta nobis est eligendi optio cumque
+                  nihil impedit quo minus id quod maxime placeat facere
+                  possimus, omnis voluptas assumenda est, omnis dolor
+                  repellendus. Temporibus autem quibusdam et aut officiis
+                  debitis aut rerum necessitatibus saepe eveniet ut et
+                  voluptates repudiandae sint et molestiae non recusandae.
+                  Itaque earum rerum hic tenetur a sapiente delectus, ut aut
+                  reiciendis voluptatibus maiores alias consequatur aut
+                  perferendis doloribus asperiores repellat.
+                </p>
+              </article>
             </div>
           </div>
         </div>
