8549ad3987ed0540b74d5832cb7f475dabbcec47
rroyson <frontiersons@gmail.com>
2017-09-12 20:00:38 -0400

    state to form, added date picker, all but submit/email finished

--- src/pages/contact-form.js
+++ src/pages/contact-form.js
@@ -1,77 +1,149 @@
 import React from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import 'react-datepicker/dist/react-datepicker.css'
 import { Link } from 'react-router-dom'
 import Header from '../containers/header'
 import HomeFooter from '../containers/home-footer'
 import { TextField, Button } from 't63'
+import { connect } from 'react-redux'
+import {
+  SET_FORM_NAME,
+  SET_FORM_ADDRESS,
+  SET_FORM_EMAIL,
+  SET_FORM_VENUE_NAME,
+  SET_FORM_VENUE_LOCATION,
+  SET_FORM_COMMENTS,
+  SET_FORM_DATE_CHANGE,
+  SET_FORM_DATE_SELECT
+} from '../constants'
 
-const Form = function() {
-  return (
-    <div>
+class Form extends React.Component {
+  render() {
+    const props = this.props
+    return (
+      <div>
 
-      <main className="athelas">
-        <div>
+        <main className="athelas">
+          <div>
 
-          <div
-            className=" vh-100 w-100 cover overflow-scroll bg-dark-gray white flex flex-column"
-            style={{
-              backgroundImage:
-                'url("https://cdn.pixabay.com/photo/2017/06/15/14/29/oak-trees-2405592_960_720.jpg")'
-            }}
-          >
-            <div>
-              <Header />
-
-            </div>
-
-            <div className="flex vh-100" />
             <div
-              className="mt7 w-100 bg-dark-gray white flex-column"
+              className=" vh-100 w-100 cover overflow-scroll bg-dark-gray white flex flex-column"
               style={{
                 backgroundImage:
-                  'url("http://res.cloudinary.com/dq832axtv/image/upload/v1505245852/vintage-concrete_rzuvfw.png")'
+                  'url("https://cdn.pixabay.com/photo/2017/06/15/14/29/oak-trees-2405592_960_720.jpg")'
               }}
             >
-              <div className="tc black-70">
-                <h1> Contact Us </h1>
+              <div>
+                <Header />
+
               </div>
-              <div className="center flex-column pt4 w-75 tc bg-white-80 o-60 black avenir">
-                <form className="tl ph2">
-                  <TextField name="Full Name" />
-                  <TextField name="Address" />
-                  <TextField name="E-mail" />
-                  <TextField name="Venue Name" />
-                  <TextField name="Venue Location" />
-                  <TextField name="Date of Event" />
-                  <textarea
-                    rows="4"
-                    cols="50"
-                    name="comments"
-                    placeholder="Any special requests/comments?"
-                  />
-                  <div className="pa4">
-                    <div className="tc">
-                      <Button className="w-50 bg-green ba br2 b--light-green">
-                        Submit
-                      </Button>
+
+              <div className="flex vh-100" />
+              <div
+                className="mt7 w-100 bg-dark-gray white flex-column"
+                style={{
+                  backgroundImage:
+                    'url("http://res.cloudinary.com/dq832axtv/image/upload/v1505245852/vintage-concrete_rzuvfw.png")'
+                }}
+              >
+                <div className="tc black-70">
+                  <h1> Contact Us </h1>
+                </div>
+                <div className="center flex-column pt4 w-75 tc bg-white-80 o-60 black avenir">
+                  <form className="tl ph2">
+                    <TextField
+                      name="Full Name"
+                      value={props.form.name}
+                      onChange={props.handleName}
+                    />
+                    <TextField
+                      name="Address"
+                      value={props.form.address}
+                      onChange={props.handleAddress}
+                    />
+                    <TextField
+                      name="E-mail"
+                      type="email"
+                      value={props.form.email}
+                      onChange={props.handleEmail}
+                    />
+                    <TextField
+                      name="Venue Name"
+                      value={props.form.venueName}
+                      onChange={props.handleVenueName}
+                    />
+                    <TextField
+                      name="Venue Location"
+                      value={props.form.venueLocation}
+                      onChange={props.handleVenueLocation}
+                    />
+                    <div className="tl pb2">
+                      <DatePicker
+                        selected={props.form.date}
+                        onChange={props.handleDateChange}
+                      />
                     </div>
-                    <div className="tc">
-                      <Button className="w-50 ba br2 ">
-                        Cancel
-                      </Button>
+                    <textarea
+                      value={props.form.comments}
+                      onChange={props.handleComments}
+                      rows="4"
+                      cols="50"
+                      name="comments"
+                      placeholder="Any special requests/comments?"
+                    />
+                    <div className="pa4">
+                      <div className="tc">
+                        <Button className="w-50 bg-green ba br2 b--light-green">
+                          Submit
+                        </Button>
+                      </div>
+                      <div className="tc">
+                        <Button className="w-50 ba br2 ">
+                          Cancel
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </form>
-              </div>
-              <div className="vh-25 va-bottom">
-                <HomeFooter />
+                  </form>
+                </div>
+                <div className="vh-25 va-bottom">
+                  <HomeFooter />
+                </div>
               </div>
             </div>
+            <div className="flex justify-center " />
           </div>
-          <div className="flex justify-center " />
-        </div>
-      </main>
-    </div>
-  )
+        </main>
+      </div>
+    )
+  }
 }
 
-export default Form
+const connector = connect(mapStateToProps, mapActionsToProps)
+
+function mapActionsToProps(dispatch) {
+  return {
+    handleName: e => dispatch({ type: SET_FORM_NAME, payload: e.target.value }),
+    handleAddress: e =>
+      dispatch({ type: SET_FORM_ADDRESS, payload: e.target.value }),
+    handleEmail: e =>
+      dispatch({ type: SET_FORM_EMAIL, payload: e.target.value }),
+    handleVenueName: e =>
+      dispatch({ type: SET_FORM_VENUE_NAME, payload: e.target.value }),
+    handleVenueLocation: e =>
+      dispatch({ type: SET_FORM_VENUE_LOCATION, payload: e.target.value }),
+    handleComments: e =>
+      dispatch({ type: SET_FORM_COMMENTS, payload: e.target.value }),
+    handleDateChange: date =>
+      dispatch({ type: SET_FORM_DATE_SELECT, payload: date })
+  }
+}
+
+function mapStateToProps(state) {
+  console.log('state', state.form.date)
+  return {
+    form: state.form
+  }
+}
+
+export default connector(Form)
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import moment from 'moment'
 import thunk from 'redux-thunk'
 import { merge } from 'ramda'
 
 const store = createStore(
   combineReducers({
-    app
+    app,
+    form
   }),
   applyMiddleware(thunk)
 )
@@ -12,13 +14,43 @@ const store = createStore(
 export default store
 
 // reducers
-function app(
-  state = { title: 'The Charleston Wedding Band' },
-  action
-) {
+function app(state = { title: 'The Charleston Wedding Band' }, action) {
   switch (action.type) {
     case 'SET_APP_TITLE':
       return merge(state, { title: action.payload })
+    default:
+      return state
+  }
+}
+
+function form(
+  state = {
+    name: '',
+    address: '',
+    email: '',
+    venueName: '',
+    venueLocation: '',
+    comments: '',
+    date: moment(),
+    dateChange: ''
+  },
+  action
+) {
+  switch (action.type) {
+    case 'SET_FORM_NAME':
+      return merge(state, { name: action.payload })
+    case 'SET_FORM_ADDRESS':
+      return merge(state, { address: action.payload })
+    case 'SET_FORM_EMAIL':
+      return merge(state, { email: action.payload })
+    case 'SET_FORM_VENUE_NAME':
+      return merge(state, { venueName: action.payload })
+    case 'SET_FORM_VENUE_LOCATION':
+      return merge(state, { venueLocation: action.payload })
+    case 'SET_FORM_COMMENTS':
+      return merge(state, { comments: action.payload })
+    case 'SET_FORM_DATE_SELECT':
+      return merge(state, { date: action.payload })
     default:
       return state
   }
